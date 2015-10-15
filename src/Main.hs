1685d7d41167dde3407938fa899d5e3960a77fee
Brian W Bush <b.w.bush@acm.org>
2015-10-14 21:36:58 -0600

    cleaned up imports and refactored functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,54 @@
-module Main where
+module Main (
+  main
+) where
 
 
-import Control.Applicative ((<$>), (<*>))
-import Data.IORef
-import Data.Maybe (catMaybes)
-import Data.Word
-import Debug.Trace (trace)
-import Graphics.UI.GLUT
+import Data.IORef (IORef, newIORef)
+import Data.Word (Word32)
+import Graphics.Rendering.OpenGL.GL (GLfloat)
+import Graphics.Rendering.OpenGL.GL.BeginEnd (PrimitiveMode(Quads), renderPrimitive)
+import Graphics.Rendering.OpenGL.GL.CoordTrans (Position(..), Size(..), viewport)
+import Graphics.Rendering.OpenGL.GL.DataType (DataType(UnsignedInt8888Rev))
+import Graphics.Rendering.OpenGL.GL.Framebuffer (ClearBuffer(..), clear)
+import Graphics.Rendering.OpenGL.GL.PerFragment (ComparisonFunction(Less), depthFunc)
+import Graphics.Rendering.OpenGL.GL.PixelData (PixelData(..))
+import Graphics.Rendering.OpenGL.GL.PixelFormat (PixelFormat(BGRA))
+import Graphics.Rendering.OpenGL.GL.PixelRectangles.Rasterization (drawPixels)
+import Graphics.Rendering.OpenGL.GL.RasterPos (windowPos)
+import Graphics.Rendering.OpenGL.GL.StateVar (($=), ($~!), get)
+import Graphics.Rendering.OpenGL.GL.Tensor (Vertex2(..), Vertex3(..))
+import Graphics.Rendering.OpenGL.GL.VertexSpec (vertex)
+import Graphics.UI.GLUT.Begin (mainLoop)
+import Graphics.UI.GLUT.Callbacks.Global (idleCallback)
+import Graphics.UI.GLUT.Callbacks.Window (DisplayCallback, displayCallback)
+import Graphics.UI.GLUT.Initialization (DisplayMode(..), getArgsAndInitialize, initialDisplayMode)
+import Graphics.UI.GLUT.Window (createWindow, postRedisplay, swapBuffers)
 
-import qualified Data.Vector.Storable as V
+import qualified Data.Vector.Storable as V (Vector, fromList, unsafeWith)
+
+
+initDLP :: IO (IORef Bool)
+initDLP = newIORef False
+
+
+drawDLP :: IORef Bool -> IO ()
+drawDLP leftFrame =
+  do
+    (Position x0 y0, Size w _) <- get viewport
+    leftFrame' <- get leftFrame
+    let
+      red       = 0x00FF0000
+      greenBlue = 0x0000FFFF
+      pixels :: V.Vector Word32
+      pixels =
+        V.fromList
+          $ replicate (fromIntegral w)
+          $ if leftFrame' then red else greenBlue
+    windowPos $ Vertex2 x0 y0
+    V.unsafeWith pixels
+      $ drawPixels (Size w 1)
+      . PixelData BGRA UnsignedInt8888Rev
+    leftFrame $~! not
 
 
 main :: IO ()
@@ -16,26 +56,20 @@ main =
   do
     (_progName, _args) <- getArgsAndInitialize
     initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
-    frameNo <- newIORef 1
-    _window <- createWindow "Parallel Planes"
+    _window <- createWindow "DLP Stereo OpenGL Example"
+    leftFrame <- initDLP
+    displayCallback $= display leftFrame
+    idleCallback $= Just (postRedisplay Nothing)
     depthFunc $= Just Less 
-    displayCallback $= display frameNo
-    idleCallback $= Just idle
     mainLoop
 
 
-idle :: IdleCallback
-idle = postRedisplay Nothing
-
-
-display :: IORef Int -> DisplayCallback
-display frameNo =
+display :: IORef Bool -> DisplayCallback
+display leftFrame =
   do
-    (Position x0 y0, Size w _) <- get viewport
-    frameNo $~! (+1)
-    frameNo' <- get frameNo
+    leftFrame' <- get leftFrame
     let
-      offset = if frameNo' `mod` 2 == 0 then 0.1 else 0
+      offset = if leftFrame' then -0.05 else 0.05
     clear [ColorBuffer, DepthBuffer]
     renderPrimitive Quads
       $ sequence_
@@ -45,25 +79,8 @@ display frameNo =
       , vertex3f (offset + 0.5,  0.5, 0.5)
       , vertex3f (offset + 0.5, -0.5, 0.5)
       ]
-    let
-      pxls :: V.Vector Word32
-      pxls = V.fromList $ replicate (fromIntegral w) $ if frameNo' `mod` 2 == 0 then red else greenBlue
-    windowPos $ Vertex2 x0 y0
-    V.unsafeWith pxls $ \ptr ->
-      drawPixels (Size w 1) $ PixelData BGRA UnsignedInt8888Rev ptr
+    drawDLP leftFrame
     swapBuffers
-
-
-red :: Word32
-red = 0x00FF0000
-
-
-greenBlue :: Word32
-greenBlue = 0x0000FFFF
-
-
-color3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
-color3f (r, g, b) = color $ Color3 r g b
 
 
 vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
